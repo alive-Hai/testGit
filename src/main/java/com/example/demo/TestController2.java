f0b46cb5d774ae66938475fd2f2c733807721661
xing <593795214@qq.com>
2022-05-21 23:16:37 +0800

    test1
    add function t1

--- src/main/java/com/example/demo/TestController2.java
+++ src/main/java/com/example/demo/TestController2.java
@@ -6,4 +6,8 @@ public class TestController2 {
     public static void main(String[] args) {
         System.out.println("feature2");
     }
+
+    public void t1(String s,String g){
+        System.out.println(s+g);
+    }
 }
