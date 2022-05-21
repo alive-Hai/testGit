376136f8fdd8ab7b3baf7ac2783f6a90c821dda8
xing <593795214@qq.com>
2022-05-21 23:06:59 +0800

    test1

---
+++ src/main/java/com/example/demo/TestController2.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import java.sql.SQLOutput;
+
+public class TestController2 {
+    public static void main(String[] args) {
+        System.out.println("feature2");
+    }
+}
